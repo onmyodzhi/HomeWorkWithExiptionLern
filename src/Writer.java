279c1fa012b81d53f5ece0bd0664ad7673d9d5ee
Alexandr <mrkobalt18@gmail.com>
2023-07-31 01:20:48 +0300

    fixed

--- src/Writer.java
+++ src/Writer.java
@@ -4,7 +4,7 @@ import java.io.IOException;
 public class Writer {
     public void writeInFile(FileCreator fileCreator,PersonListCreator listCreator){
         try(FileWriter writer = new FileWriter(fileCreator.getDb(), true)){
-            writer.write(listCreator.getDbList().toString());
+            writer.write(listCreator.getDbList().toString() + '\n');
         }catch (IOException e){
             System.err.println(e.getMessage());
         }
